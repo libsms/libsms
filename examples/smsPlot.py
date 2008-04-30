84866bdfd74f033b7b2ef1016dd468a667567f31
Rich Eakin <reakinator@gmail.com>
2008-04-30 19:02:21 +0000

    took out stft (now in stft.py), now using pylab.specgram.  Started track plot.

--- examples/smsPlot.py
+++ examples/smsPlot.py
@@ -1,74 +1,72 @@
 #!/usr/bin/env python
+
+import aifc
 from pylab import *
-import aifc, yaml, numpy
+from numpy import *
+# using the libyaml loader is much faster, if available
+from yaml import load, dump
+try:
+    from yaml import CLoader as Loader
+    from yaml import CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
 
-soundFileName = 'audio/piano.aiff'
-soundFileName = 'audio/piano.aiff'
-yamlFileName = 'piano5.yaml'
+soundFilename = 'audio/piano.aiff'
+yamlFilename = 'piano5.yaml'
 
-print 'reading', soundFileName, '...'
-sf = aifc.open(soundFileName, 'r')
 
-    
+sf = aifc.open(soundFilename, 'r')
+
 bits = sf.getsampwidth()*8
 srate = sf.getframerate()
 nframes = sf.getnframes()
 
-if sf.getnchannels() == 2:
-    sys.exit("stereo file not yet supported, TODO: mix-to-mono")
-
 # get data and convert to unsigned int from hex string
-sfdata = numpy.fromstring(sf.readframes(nframes), 'H')
+sfdata = fromstring(sf.readframes(nframes), 'H')
 # byteswap and normalize to 0:+2^bits
 sfdata = sfdata.byteswap() - 2**bits / 2
 # convert to float and normalize to -1:1
 sfdata = array(sfdata, float) / (.5 * 2**bits) - 1
 
-#now plot a spectragram of sfdata
+# plot a spectragram of sound
+ymax = 5000
+plotTitle = 'sample:', soundFilename
 
-noverlap=256
-sizeWindow=512
-nfft=2048
-dbclip=100
-
-window=hamming(sizeWindow)
-
-if nframes < sizeWindow :
-    sdata = hstack([ x, zeros(sizeWindow - nframes)])
-
-Modd = mod(sizeWindow,2) # 0 if sizeWindow even, 1 if odd
-Mo2 = (sizeWindow-Modd)/2
-zp = zeros(nfft-sizeWindow)
-wzp = hstack([ window[Mo2+1:sizeWindow], zp, window[1:Mo2] ]) 
-
-if noverlap<0 :
-    nhop = - noverlap
-    noverlap = sizeWindow - nhop
-else
-    nhop = sizeWindow - noverlap
-
-frames = 1 + floor((nframes-noverlap)/nhop)
-X = zeros((nfft, frames))
-xoff = 0;
-for i in xrange(frames-1):
-    xframe = array(sfdata[xoff:xoff+sizeWindow])
-    xoff = xoff + nhop
-    xzp = hstack([xframe[Mo2+1:sizeWindow], zp, xframe[1:Mo2] ])
-    xw = wzp * xzp
-    
+Pxx, freqs, bins, im = specgram(sfdata, NFFT=1024, Fs= srate, noverlap=900)
+title(plotTitle)
+xlabel('time (seconds)')
+ylabel('frequency (hertz)')
+axes().set_ylim(0,ymax)
+show()
 
 
-#NFFT = 1024       # the length of the windowing segments
-# Pxx is the segments x freqs array of instantaneous power, freqs is
-# the frequency vector, bins are the centers of the time bins in which
-# the power is computed, and im is the matplotlib.image.AxesImage
-# instance
-
-#Pxx, freqs, bins, im = specgram(sfdata, NFFT=NFFT, Fs=srate, noverlap=900)
 
 #import sms analysis data from yaml file
-print 'loading', yamlFileName, '...'
-sms = yaml.load(open(yamlFileName).read())
+#print 'loading', yamlFileName, '...'
+smsFile = yaml.load(open(yamlFileName).read(), Loader=Loader)
 
-nRecords = sms['header']['nRecords']
+nRecords = smsFile['header']['nRecords']
+nTraj = smsFile['header']['nTrajectories']
+
+smsData = smsFile['Data'] #todo: change to 'data' for future files
+
+# make a track and plot [num,[timetags],[freqs]]
+
+for i in range(nTraj): # loop for each harmonic
+    time = -1; # use -1 index to check if it has been set (which would be positive)
+    freq = 0;
+    for j in range(nRecords): # loop for all frames
+        if smsData[j]['harmonics']: # make sure frame has harmonic data
+            for num,traj in smsData[j]['harmonics'].iteritems(): #  build time and freq lists
+                if i = num:
+                    if time == -1:
+                        time = [smsData[j]['timetag']]
+                        freq = [smsData[j]['harmonics'][num][0]]
+                    else:
+                        time.append(smsData[j]['timetag'])
+                        freq.append(smsData[j]['harmonics'][num][0])
+    plot(time,freq)
+
+        
+
 
