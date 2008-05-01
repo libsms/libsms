3b674a0a2372e0bd41fe07d412cf6eff91322c60
Rich Eakin <reakinator@gmail.com>
2008-05-01 19:21:51 +0000

    re-arranging test and example folders

--- examples/smsPlot.py
+++ examples/smsPlot.py
@@ -32,7 +32,7 @@ sfdata = array(sfdata, float) / (.5 * 2**bits) - 1
 ymax = 5000
 plotTitle = 'sample:', soundFilename
 
-Pxx, freqs, bins, im = specgram(sfdata, NFFT=1024, Fs= srate, noverlap=900)
+specgram(sfdata, NFFT=1024, Fs= srate, noverlap=900)
 title(plotTitle)
 xlabel('time (seconds)')
 ylabel('frequency (hertz)')
@@ -43,7 +43,7 @@ show()
 
 #import sms analysis data from yaml file
 #print 'loading', yamlFileName, '...'
-smsFile = yaml.load(open(yamlFileName).read(), Loader=Loader)
+smsFile = load(open(yamlFilename).read(), Loader=Loader)
 
 nRecords = smsFile['header']['nRecords']
 nTraj = smsFile['header']['nTrajectories']
@@ -51,15 +51,15 @@ nTraj = smsFile['header']['nTrajectories']
 smsData = smsFile['Data'] #todo: change to 'data' for future files
 
 # make a track and plot [num,[timetags],[freqs]]
-
 for i in range(nTraj): # loop for each harmonic
     time = -1; # use -1 index to check if it has been set (which would be positive)
     freq = 0;
     for j in range(nRecords): # loop for all frames
         if smsData[j]['harmonics']: # make sure frame has harmonic data
             for num,traj in smsData[j]['harmonics'].iteritems(): #  build time and freq lists
-                if i = num:
+                if i == num:
                     if time == -1:
+                        print 'setting first frame'
                         time = [smsData[j]['timetag']]
                         freq = [smsData[j]['harmonics'][num][0]]
                     else:
--- python/sms.i
+++ python/sms.i
@@ -1,6 +1,6 @@
 %module sms
 %{
+#define SWIG_FILE_WITH_INIT
 #include "../src/sms.h"
 %}
 
-%include "../src/sms.h"
