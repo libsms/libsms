7f149dbb6f5782433a66a62d76c2d5c0d05e2fe2
Rich Eakin <reakinator@gmail.com>
2008-10-07 14:47:14 +0000

    doxygen documentation and removed debug code

--- src/windows.c
+++ src/windows.c
@@ -18,26 +18,32 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * 
  */
+/*! \file windows.c
+ * \brief functions for creating various windows
+ * 
+ * Use sms_getWindow() for selecting which window will be made
+ */
 #include "sms.h"
 
-/* window to be used in the IFFT synthesis */
-
+/*! \brief window to be used in the IFFT synthesis
+ * 
+ * contains both an inverse Blackman-Harris and triangular
+ * \todo improve this documentation with references and explanations
+ * \param sizeWindow the size of the window
+ * \param pFWindow pointer to an array that will hold the window
+ */
 void IFFTwindow (int sizeWindow, float *pFWindow)
 {
 
 	int     i;
 	float a0 = .35875, a1 = .48829, a2 = .14128, a3 = .01168;
 	double fConst = TWO_PI / sizeWindow, fIncr = 2.0 /sizeWindow, fVal = 0;
-// RTE DEBUG ///////////////////////////////////////
-        FILE *dd;
-        dd = fopen("debug.txt", "w");
-  
+
 	/* compute inverse of window */
 	for(i = 0; i < sizeWindow; i++) 
 	{
 		pFWindow[i] = 1 / (a0 - a1 * cos(fConst * i) +
 			a2 * cos(fConst * 2 * i) - a3 * cos(fConst * 3 * i));
-                fprintf(dd, "%f ", pFWindow[i]);
 	}
   
 	/* scale function by a triangular */
@@ -51,15 +57,13 @@ void IFFTwindow (int sizeWindow, float *pFWindow)
 		pFWindow[i] = fVal * pFWindow[i]  / 2.787457;
 		fVal -= fIncr;
 	}
-/* 	for(i = 0; i < sizeWindow; i++)  */
-/*                 fprintf(dd, "%f ", pFWindow[i]); */
-        fclose(dd);
-        
 }
 
-/* function to create a backmanHarris window
- * int     sizeWindow;   window size
- * float  *pFWindow;      window array
+/*! \brief BlackmanHarris window with 62dB rolloff
+ * 
+ * \todo where did these come from?
+ * \param sizeWindow the size of the window
+ * \param pFWindow pointer to an array that will hold the window
  */
 void BlackmanHarris62 (int sizeWindow, float *pFWindow)
 {
@@ -84,9 +88,10 @@ void BlackmanHarris62 (int sizeWindow, float *pFWindow)
 		pFWindow[i] = pFWindow[i] / fSum;
 }
 
-/* function to create a backmanHarris window
- * int     sizeWindow;   window size
- * float  *pFWindow;      window array
+/*! \brief BlackmanHarris window with 70dB rolloff
+ * 
+ * \param sizeWindow the size of the window
+ * \param pFWindow pointer to an array that will hold the window
  */
 void BlackmanHarris70 (int sizeWindow, float *pFWindow)
 {
@@ -96,23 +101,13 @@ void BlackmanHarris70 (int sizeWindow, float *pFWindow)
 	float a0 = .42323, a1 = .49755, a2 = .07922;
 	double fConst = TWO_PI / sizeWindow;
 
-// RTE DEBUG ///////////////////////////////////////
-/*         FILE *dd; */
-/*         dd = fopen("debug.txt", "w"); */
-/*         static float max = 0.; */
-  
 	/* compute window */
 	for(i = 0; i < sizeWindow; i++) 
 	{
 		fSum += pFWindow[i] = a0 - a1 * cos(fConst * i) +
 			a2 * cos(fConst * 2 * i);
-/*                 fprintf(dd, "%f ", pFWindow[i]); */
 	}
 
-/*         fclose(dd); */
-
-
-	/* I do not know why I now need this factor of two */
 	fSum = fSum / 2;
   
 	/* scale function */
@@ -122,9 +117,10 @@ void BlackmanHarris70 (int sizeWindow, float *pFWindow)
         }
 }
 
-/* function to create a backmanHarris window
- * int     sizeWindow;   window size
- * float  *pFWindow;      window array
+/*! \brief BlackmanHarris window with 74dB rolloff
+ * 
+ * \param sizeWindow the size of the window
+ * \param pFWindow pointer to an array that will hold the window
  */
 void BlackmanHarris74 (int sizeWindow, float *pFWindow)
 {
@@ -149,9 +145,10 @@ void BlackmanHarris74 (int sizeWindow, float *pFWindow)
 		pFWindow[i] = pFWindow[i] / fSum;
 }
 
-/* function to create a backmanHarris window
- * int     sizeWindow;   window size
- * float  *pFWindow;      window array
+/*! \brief BlackmanHarris window with 92dB rolloff
+ * 
+ * \param sizeWindow the size of the window
+ * \param pFWindow pointer to an array that will hold the window
  */
 void BlackmanHarris92 (int sizeWindow, float *pFWindow)
 {
@@ -176,14 +173,20 @@ void BlackmanHarris92 (int sizeWindow, float *pFWindow)
 		pFWindow[i] = pFWindow[i] / fSum;
 }
 
+/*! \brief default BlackmanHarris window (70dB rolloff)
+ * 
+ * \param sizeWindow the size of the window
+ * \param pFWindow pointer to an array that will hold the window
+ */
 void BlackmanHarris (int sizeWindow, float *pFWindow)
 {
 	 BlackmanHarris70 (sizeWindow, pFWindow);
 }
 
-/* function to design a Hamming window
- * int     sizeWindow;   window size
- * float  *pFWindow;      window array
+/*! \brief Hamming window
+ *
+ * \param sizeWindow   window size
+ * \param pFWindow      window array
  */
 void Hamming (int sizeWindow, float *pFWindow)
 {
@@ -199,9 +202,10 @@ void Hamming (int sizeWindow, float *pFWindow)
 		pFWindow[i] /= fSum;
 }
 
-/* function to design a Hanning window
- * int     sizeWindow;   window size
- * float  *pFWindow;      window array
+/*! \brief Hanning window
+ *
+ * \param sizeWindow   window size
+ * \param pFWindow      window array
  */
 void Hanning (int sizeWindow, float *pFWindow)
 {
@@ -211,7 +215,13 @@ void Hanning (int sizeWindow, float *pFWindow)
     pFWindow[i] = (sin(PI*i/(sizeWindow-1)))*(sin(PI*i/(sizeWindow-1)));
 }
 
-
+/*! \brief main function for getting various windows
+ *
+ * \see SMS_WINDOWS for the different window types available
+ * \param sizeWindow   window size
+ * \param pFWindow      window array
+ * \param iWindowType the desired window type defined by SMS_WINDOWS 
+ */
 void sms_getWindow (int sizeWindow, float *pFWindow, int iWindowType)
 {
 	switch (iWindowType)
