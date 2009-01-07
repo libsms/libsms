d05e55cf3d5026f64d0ebdea55da043c87535316
Rich Eakin <reakinator@gmail.com>
2009-01-07 16:04:51 +0000

    switched sms_dft() for sms_fft() and sms_ifft().  Removed FFTW code

--- src/spectrum.c
+++ src/spectrum.c
@@ -52,62 +52,6 @@ int sms_spectrum (float *pFWaveform, int sizeWindow, float *pFMagSpectrum,
         float fReal, fImag;
 	static int iOldSizeWindow = 0;
   
-#ifdef FFTW
-        static int fftwPlanned = 0;
-
-	/* compute window when necessary and re-plan fftw*/
-	if (iOldSizeWindow != sizeWindow) 
-        {       
-                sms_getWindow (sizeWindow, pAnalParams->pFSpectrumWindow, pAnalParams->iWindowType);
-
-                if(fftwPlanned)
-                        fftwf_destroy_plan(pAnalParams->fftw.plan);
-
-                if((pAnalParams->fftw.plan =  fftwf_plan_dft_r2c_1d( sizeFft, pAnalParams->fftw.pWaveform,
-                                                                   pAnalParams->fftw.pSpectrum, FFTW_ESTIMATE)) == NULL)
-
-                {
-                        printf("Spectrum: could not make fftw plan of size: %d \n", sizeFft);
-                        return(-1); 
-                }
-                fftwPlanned = 1;
-        }
-	iOldSizeWindow = sizeWindow;
-
-        /*! \todo why is this memset necessary if the waveform is overwritten below? */
-        memset(pAnalParams->fftw.pWaveform, 0, sizeFft * sizeof(float));
-
-	/* apply window to waveform and center window around 0 */
-	iOffset = sizeFft - (iMiddleWindow - 1);
-	for (i=0; i<iMiddleWindow-1; i++)
-		pAnalParams->fftw.pWaveform[iOffset + i] =  pAnalParams->pFSpectrumWindow[i] * pFWaveform[i];
-	iOffset = iMiddleWindow - 1;
-	for (i=0; i<iMiddleWindow; i++)
-		pAnalParams->fftw.pWaveform[i] = pAnalParams->pFSpectrumWindow[iOffset + i] * pFWaveform[iOffset + i];
-
-        fftwf_execute(pAnalParams->fftw.plan);
-
-	/* convert from rectangular to polar coordinates */
-	for (i = 1; i < sizeMag; i++) 
-	{
-		fReal = pAnalParams->fftw.pSpectrum[i][0];
-		fImag = pAnalParams->fftw.pSpectrum[i][1];
-      
-		if (fReal != 0 || fImag != 0)
-		{
-			pFMagSpectrum[i] = sms_magToDB (sqrt (fReal * fReal + fImag * fImag));
-                        /* \todo the phase spectrum is inverted! find out why.. */
-			/*pFPhaseSpectrum[i] = atan2 (-fImag, fReal); */
-			pFPhaseSpectrum[i] = -atan2 (-fImag, fReal);
-		}
-	}
-        /*DC and Nyquist  \todo might not be necessary? */
-        fReal = pAnalParams->fftw.pSpectrum[0][0];
-        fImag = pAnalParams->fftw.pSpectrum[sizeMag][0];
-        pFMagSpectrum[0] = sms_magToDB (sqrt (fReal * fReal + fImag * fImag));
-        pFPhaseSpectrum[0] = atan2(-fImag, fReal);
-
-#else /* using OOURA */        
 	if (iOldSizeWindow != sizeWindow) 
         {
                 sms_getWindow (sizeWindow, pAnalParams->pFSpectrumWindow, pAnalParams->iWindowType);
@@ -118,11 +62,10 @@ int sms_spectrum (float *pFWaveform, int sizeWindow, float *pFMagSpectrum,
         float *pFBuffer;
 	/* allocate buffer */    
 
-
 	if ((pFBuffer = (float *) calloc(sizeFft, sizeof(float))) == NULL)
 		return -1;
 
-	/* apply window to waveform and center window around 0 */
+	/* apply window to waveform and center window around 0 (zero-phase windowing)*/
 	iOffset = sizeFft - (iMiddleWindow - 1);
 	for (i=0; i<iMiddleWindow-1; i++)
 		pFBuffer[iOffset + i] =  pAnalParams->pFSpectrumWindow[i] * pFWaveform[i];
@@ -130,7 +73,7 @@ int sms_spectrum (float *pFWaveform, int sizeWindow, float *pFMagSpectrum,
 	for (i=0; i<iMiddleWindow; i++)
 		pFBuffer[i] = pAnalParams->pFSpectrumWindow[iOffset + i] * pFWaveform[iOffset + i];
   
-        sms_rdft(sizeFft, pFBuffer, 1);
+        sms_fft(sizeFft, pFBuffer);
   
 	/* convert from rectangular to polar coordinates */
 	for (i = 0; i < sizeMag; i++)
@@ -147,7 +90,6 @@ int sms_spectrum (float *pFWaveform, int sizeWindow, float *pFMagSpectrum,
 	}
 
 	free (pFBuffer);
-#endif/*FFTW*/
         
 	return (sizeMag);
 }
@@ -167,50 +109,10 @@ int sms_quickSpectrum (float *pFWaveform, float *pFWindow, int sizeWindow,
 {
 	int sizeMag = sizeFft >> 1, i;
 	float fReal, fImag;
-
-#ifdef FFTW
-        /*! \todo same as todo in sms_spectrum */
-        static SMS_Fourier fftData;
-        /* \todo memory leak here.. */
-        fftData.pWaveform = fftwf_malloc(sizeof(float) * sizeFft);
-        fftData.pSpectrum = fftwf_malloc(sizeof(fftwf_complex) * (sizeFft / 2 + 1));
-        fftData.plan =  fftwf_plan_dft_r2c_1d( sizeFft, fftData.pWaveform,
-                                               fftData.pSpectrum, FFTW_ESTIMATE);
-
-        memset(fftData.pWaveform, 0, sizeFft * sizeof(float));
-
-	/* apply window to waveform */
-	for (i = 0; i < sizeWindow; i++)
-                fftData.pWaveform[i] =  pFWindow[i] * pFWaveform[i];
-
-        fftwf_execute(fftData.plan);
-
-	/*convert from rectangular to polar coordinates*/
-	for (i = 1; i < sizeMag; i++)
-	{
-		fReal = fftData.pSpectrum[i][0];
-		fImag = fftData.pSpectrum[i][1];
-      
-		if (fReal != 0 || fImag != 0)
-		{
-			pFMagSpectrum[i] = sqrt (fReal * fReal + fImag * fImag);
-                        /* \todo this should be checked out of the for loop.., only once */
-			if(pFPhaseSpectrum)
-                                pFPhaseSpectrum[i] = atan2 (fImag, fReal); /*should be negative like above? */
-		}
-	}
-        /*DC and Nyquist  */
-        fReal = fftData.pSpectrum[0][0];
-        fImag = fftData.pSpectrum[sizeMag][0];
-        pFMagSpectrum[0] = sqrt (fReal * fReal + fImag * fImag);
-        if (pFPhaseSpectrum)
-                pFPhaseSpectrum[0] = atan2(fImag, fReal);
-
-#else /* using OOURA */
-  
         int it2;
         float *pFBuffer;
 	/* allocate buffer */    
+        /* \todo why cannot the fft just be done on pFWaveform in-place? */
 	if ((pFBuffer = (float *) calloc(sizeFft+1, sizeof(float))) == NULL)
 		return -1;
     
@@ -219,14 +121,12 @@ int sms_quickSpectrum (float *pFWaveform, float *pFWindow, int sizeWindow,
                 pFBuffer[i] =  pFWindow[i] * pFWaveform[i];
   
 	/* compute real FFT */
-        sms_rdft(sizeFft, pFBuffer, 1); 
+        sms_fft(sizeFft, pFBuffer); 
   
 	/* convert from rectangular to polar coordinates */
         sms_spectrumRtoP(sizeMag, pFBuffer, pFMagSpectrum, pFPhaseSpectrum);
 
 	free(pFBuffer);
-
-#endif /* FFTW */
   
 	return (sizeMag);
 }
@@ -259,7 +159,7 @@ int sms_invQuickSpectrum (float *pFMagSpectrum, float *pFPhaseSpectrum,
 		pFBuffer[it2+1] = fPower * sin (pFPhaseSpectrum[i]);
 	}
 	/* compute IFFT */
-        sms_rdft(sizeFft, pFBuffer, -1); 
+        sms_ifft(sizeFft, pFBuffer); 
  
 	/* assume the output array has been taken care off */
 	for (i = 0; i < sizeWave; i++)
@@ -271,7 +171,7 @@ int sms_invQuickSpectrum (float *pFMagSpectrum, float *pFPhaseSpectrum,
 }
  
 /*! \brief function for a quick inverse spectrum, windowed
- * \todo move fftwf_execture into here
+ * \todo move fftwf_execute into here
  * function to perform the inverse FFT, windowing the output
  * float *pFMagSpectrum        input magnitude spectrum
  * float *pFPhaseSpectrum      input phase spectrum
@@ -300,7 +200,7 @@ int sms_invQuickSpectrumW (float *pFMagSpectrum, float *pFPhaseSpectrum,
 		pFBuffer[it2+1] = fPower * sin (pFPhaseSpectrum[i]);
 	}    
 	/* compute IFFT */
-        sms_rdft(sizeFft, pFBuffer, -1); 
+        sms_ifft(sizeFft, pFBuffer); 
 
  	/* assume the output array has been taken care off */
         /* \todo is a seperate pFBuffer necessary here?
@@ -341,11 +241,11 @@ void sms_spectrumRtoP( int sizeMag, float *pFReal, float *pFMag, float *pFPhase)
 
 /*! \brief compute magnitude spectrum of a DFT
  *              
- * \param sizeMag	       size of 
+ * \param sizeMag	       size of output Magnitude (half of input real FFT)
  * \param pFReal	       pointer to input FFT real array (real/imag floats)
  * \param pFMAg	       pointer to float array of magnitude spectrum
  */
-void sms_spectrumMag( int sizeMag, float *pFReal, float *pFMag)
+void sms_spectrumMag( int sizeMag, float *pInRect, float *pOutMag)
 {
         int i, it2;
         float fReal, fImag;
@@ -353,10 +253,8 @@ void sms_spectrumMag( int sizeMag, float *pFReal, float *pFMag)
 	for (i=0; i<sizeMag; i++)
 	{
 		it2 = i << 1;
-		fReal = pFReal[it2];
-		fImag = pFReal[it2+1];
-      
-                pFMag[i] = sqrtf(fReal * fReal + fImag * fImag);
+		fReal = pInRect[it2];
+		fImag = pInRect[it2+1];
+                pOutMag[i] = sqrtf(fReal * fReal + fImag * fImag);
 	}
-
 }
