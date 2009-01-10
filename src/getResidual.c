24a2908c7dde670e48ed336cc7bef30bd64d85f1
Rich Eakin <reakinator@gmail.com>
2009-01-10 18:01:19 +0000

    lots of changes, trying to fix memory allocation.  The window array is now passed in with a pointer.

--- src/getResidual.c
+++ src/getResidual.c
@@ -19,146 +19,40 @@
  * 
  */
 /*! \file getResidual.c
- * \brief main get_residual function, along with helper filter functions
+ * \brief main sms_residual() function
  */
 
 #include "sms.h"
 
-/*! \brief  function to implement a pole-zero filter
- * 
- * \param pFa        pointer to numerator coefficients
- * \param pFb        pointer to denominator coefficients
- * \param nCoeff    number of coefficients
- * \param fInput     input sample
- * \param pD          pointer to filter coefficients 
- * \return value is the  filtered sample 
- */
-static float ZeroPoleFilter (float *pFa, float *pFb, int nCoeff, float fInput,
-                             float *pD)
-{
-	double fOut = 0;
-	int iSection;
-    
-	pD[0] = fInput;
-	for (iSection = nCoeff-1; iSection > 0; iSection--)
-	{
-		fOut = fOut + pFa[iSection] * pD[iSection];
-		pD[0] = pD[0] - pFb[iSection] * pD[iSection];
-		pD[iSection] = pD [iSection-1];
-	}
-	fOut = fOut + pFa[0] * pD[0];
-	return((float) fOut);
-}
-
-	/* cutoff 1500 Hz */
-/*	static float pFCoeff32k[10] =  {0.679459, -2.71784, 4.07676, -2.71784, 
-		0.679459, 1, -3.23168, 3.97664, -2.20137, 0.461665};  
-	static float pFCoeff36k[10] =  {0.709489, -2.83796, 4.25694, -2.83796, 
-		0.709489, 1, -3.31681, 4.17425, -2.3574, 0.503375};
-	static float pFCoeff40k[10] =  {0.734408, -2.93763, 4.40645, -2.93763, 
-		0.734408, 1, -3.38497, 4.33706, -2.48914, 0.539355};
-	static float pFCoeff441k[10] =  {0.755893, -3.02357, 4.53536, -3.02357, 
-		0.755893, 1, -3.44205, 4.47657, -2.6043, 0.571374};
-	static float pFCoeff48k[10] =  {0.773347, -3.09339, 4.64008, -3.09339, 
-		0.773347, 1, -3.48731, 4.58929, -2.69888, 0.598065};
-*/
-
-
-/*! \brief function to filter a waveform with a high-pass filter
- * 
- *  cutoff =1500 Hz  
- * 
- * \todo this filter only works on sample rates up to 48k?
- *
- * \param pFResidual          pointer to residual signal
- * \param sizeResidual        size of signal
- * \param iSamplingRate      sampling rate of signal                                                    
- * \param pD          pointer to filter coefficients
- */
-static void FilterResidual (float *pFResidual, int sizeResidual, 
-                             int iSamplingRate, float *pD)
-{
-	/* cutoff 800Hz */
-	static float pFCoeff32k[10] =  {0.814255, -3.25702, 4.88553, -3.25702, 
-		0.814255, 1, -3.58973, 4.85128, -2.92405, 0.66301};
-	static float pFCoeff36k[10] =  {0.833098, -3.33239, 4.99859, -3.33239, 
-		0.833098, 1, -3.63528, 4.97089, -3.02934,0.694052};
-	static float pFCoeff40k[10] =  {0.848475, -3.3939, 5.09085, -3.3939, 
-		0.848475, 1, -3.67173, 5.068, -3.11597, 0.71991}; 
-	static float pFCoeff441k[10] =  {0.861554, -3.44622, 5.16932, -3.44622, 
-		0.861554, 1, -3.70223, 5.15023, -3.19013, 0.742275};
-	static float pFCoeff48k[10] =  {0.872061, -3.48824, 5.23236, -3.48824, 
-		0.872061, 1, -3.72641, 5.21605, -3.25002, 0.76049};
-	float *pFCoeff, fSample = 0;
-	int i;
-  
-	if (iSamplingRate <= 32000)
-		pFCoeff = pFCoeff32k;
-	else if (iSamplingRate <= 36000)
-		pFCoeff = pFCoeff36k;
-	else if (iSamplingRate <= 40000)
-		pFCoeff = pFCoeff40k;
-	else if (iSamplingRate <= 44100)
-		pFCoeff = pFCoeff441k;
-	else
-		pFCoeff = pFCoeff48k;
-  
-	for(i = 0; i < sizeResidual; i++)
-	{
-		/* try to avoid underflow when there is nothing to filter */
-		if (i > 0 && fSample == 0 && pFResidual[i] == 0)
-			return;
-      
-		fSample = pFResidual[i];
-		pFResidual[i] = 
-			ZeroPoleFilter (&pFCoeff[0], &pFCoeff[5], 5, fSample, pD);
-	}
-}
-
 /*! \brief get the residual waveform
  *
- * \param pFSynthesis       pointer to deterministic component
- * \param pFOriginal          pointer to original waveform
- * \param pFResidual        pointer to output residual waveform
- * \param sizeWindow       size of buffer
- * \param pAnalParams    pointer to analysis parameters
- * \return 0 if no representation, 1 if got a representation
+ * \param sizeWindow       size of buffers
+ * \param pSynthesis       pointer to deterministic component
+ * \param pOriginal          pointer to original waveform
+ * \param pResidual        pointer to output residual waveform
+ * \param pWindow    pointer to windowing array
+ * \return residual percentage (0 if residual was not large enough)
  */
-int sms_residual (float *pFSynthesis, float *pFOriginal,  
-                 float *pFResidual, int sizeWindow, SMS_AnalParams *pAnalParams)
+int sms_residual ( int sizeWindow, float *pSynthesis, float *pOriginal, float *pResidual, float *pWindow)
 {
-	static float fResidualMag = 0, fOriginalMag = 0, *pD = NULL, 
-		*pFWindow = NULL;
+	static float fResidualMag = 0, fOriginalMag = 0, *pFWindow = NULL;
 	float fScale = 1, fCurrentResidualMag = 0, fCurrentOriginalMag = 0;
 	int i;
    
-	/* allocate memory for filter coefficients */
-	if (pD == NULL)
-		pD = (float *) calloc(5, sizeof(float));	    
-
-        /* \todo why is the window made here.. why not globally or stored in SMS_AnalParams? */
-	if (pFWindow == NULL)
-	{
-		if ((pFWindow = (float *) calloc(sizeWindow, sizeof(float))) == NULL)
-			return -1;
-		//Hamming (sizeWindow, pFWindow);
-                sms_getWindow( sizeWindow, pFWindow, SMS_WIN_HAMMING);
-	}
-
 	/* get residual */
 	for (i=0; i<sizeWindow; i++)
-                pFResidual[i] = pFOriginal[i] - pFSynthesis[i];
+                pResidual[i] = pOriginal[i] - pSynthesis[i];
 
 	/* get energy of residual */
 	for (i=0; i<sizeWindow; i++)
-		fCurrentResidualMag += fabsf( pFResidual[i] * pFWindow[i]);
+		fCurrentResidualMag += fabsf( pResidual[i] * pWindow[i]);
 
 	/* if residual is big enough compute coefficients */
 	if (fCurrentResidualMag/sizeWindow > .01)
 	{  
 		/* get energy of original */
 		for (i=0; i<sizeWindow; i++)
-			fCurrentOriginalMag += fabs( pFOriginal[i] * pFWindow[i]);
+			fCurrentOriginalMag += fabs( pOriginal[i] * pWindow[i]);
   
 		fOriginalMag = 
 			.5 * (fCurrentOriginalMag/sizeWindow + fOriginalMag);
@@ -170,51 +64,10 @@ int sms_residual (float *pFSynthesis, float *pFOriginal,
 		{
 			fScale = fOriginalMag / fResidualMag;
 			for (i=0; i<sizeWindow; i++)
-				pFResidual[i] *= fScale;
-
-      
-			if (pAnalParams->iDebugMode == SMS_DBG_ALL ||
-				pAnalParams->iDebugMode == SMS_DBG_STOC_ANAL)
-				fprintf(stdout, 
-				        "getResidual: frame %d: scaled residual by %f\n", 
-				        pAnalParams->ppFrames[0]->iFrameNum, fScale);
+				pResidual[i] *= fScale;
 		}
    
-		/* store residual percentage in global variable */
-		pAnalParams->fResidualPercentage += fCurrentResidualMag / fCurrentOriginalMag;
-		if (pAnalParams->iDebugMode == SMS_DBG_ALL ||
-		    pAnalParams->iDebugMode == SMS_DBG_STOC_ANAL)
-			fprintf (stdout, "getResidual: frame %d, residual perc %f \n",
-			         pAnalParams->ppFrames[0]->iFrameNum, 
-			         fCurrentResidualMag / fCurrentOriginalMag);
-		if (pAnalParams->iDebugMode == 13)
-			fprintf (stdout, "%f\n",
-			         fCurrentResidualMag / fCurrentOriginalMag);
-
-		if (pAnalParams->iDebugMode == SMS_DBG_SYNC)
-			sms_writeDebugData (pFOriginal+sizeWindow/2, 
-			                  pFSynthesis+sizeWindow/2,
-		                    pFResidual+sizeWindow/2, sizeWindow/2);
-
-		if (pAnalParams->iDebugMode == SMS_DBG_RESIDUAL)
-		{
-			sms_createResSF( pAnalParams->iSamplingRate );
-			sms_writeResSound (pFResidual+sizeWindow/2, sizeWindow/2);
-		}
-		/* filter residual with a high pass filter (it solves some problems) */
-		FilterResidual (pFResidual, sizeWindow, pAnalParams->iSamplingRate, pD);
-
-		return (1);
-	}
-	if (pAnalParams->iDebugMode == SMS_DBG_SYNC)
-		sms_writeDebugData (pFOriginal+sizeWindow/2, 
-			                pFSynthesis+sizeWindow/2,
-		                  pFResidual+sizeWindow/2, sizeWindow/2);
-
-	if (pAnalParams->iDebugMode == SMS_DBG_RESIDUAL)
-	{
-		sms_createResSF( pAnalParams->iSamplingRate );
-		sms_writeResSound (pFResidual+sizeWindow/2, sizeWindow/2);
+		return (fCurrentResidualMag / fCurrentOriginalMag);
 	}
 	return (0);
 }
