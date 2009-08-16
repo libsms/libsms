4d57289b2c43ec183c6bba7de44ba51d569f608b
Rich Eakin <reakinator@gmail.com>
2009-08-16 13:08:35 +0000

    removed old argument parsing code.  worked a bit on verbose info printing

--- tools/smsAnal.c
+++ tools/smsAnal.c
@@ -29,193 +29,6 @@ const char *help_header_text =
         "See the man page for details.\n\n"
         "Options:\n";
 
-void usage (void)
-{
-    fprintf (stderr, "\n"
-             "Usage: smsAnal [options]  <inputSoundFile> <outputSmsFile>\n"
-             "\n"
-             "analyzes a sound file a stores it in a binary SMS file. All parameters are numbers."
-             "See the man page for details.\n\n"
-             "Options:\n"
-             "      --help (this message)\n"
-             "      -v    verbose mode (default 0, none)\n"
-             "      -d    debugMode (default 0, none)\n"
-             "      -f    format (default 0, harmonic)\n"
-             "      -q    soundType (default 0, sound phrase)\n"
-             "      -x    analysis direction (default 0, direct)\n"
-             " STFT parameters:\n"
-             "      -s    windowSize (default 3.5 periods)\n"
-             "      -i    windowType (default 1, Blackman-Harris 62 db)\n"
-             "      -r    frameRate (default 300 hz)\n"
-             " Peak Detection parameters:\n"
-             "      -j    highestFreq (default 12000 hz)\n"
-             "      -k    minPeakMag (default -100 dB)\n"
-             " Harmonic Detection parameters:\n"
-             "      -y    refHarmonic (default 1)\n"
-             "      -m    minRefHarmMag (default 30 dB)\n"
-             "      -z    refHarmDiffFromMax (default 30 dB)\n"
-             "      -u    defaultFund (default 100 hz)\n"
-             "      -l    lowestFund (default 50 hz)\n"
-             "      -h    highestFund (default 1000 hz)\n"
-             " Peak Continuation parameters:\n"
-             "      -n    nGuides (default 100)\n"
-             "      -p    nTracks (default 60)\n"
-             "      -w    freqDeviation (default .45)\n"
-             "      -t    peakContGuide (default .4)\n"
-             "      -o    fundContToGuide (default .5)\n"
-             " Track Cleaning parameters:\n"
-             "      -g    cleanTrack (default 1, yes)\n"
-             "      -a    minTrackLength (default .1 seconds)\n"
-             "      -b    maxSleepingTime (default .1 seconds)\n"
-             " Stochastic Analysis parameters:\n"
-             "      -e    stochasticType (default 1, approximated spectrum). 0=none \n"
-             "      -c    nStocCoeff (default 64)\n"
-             " Spectral Envelope parameters:\n"
-             "      -blahhhhhh don't have any more room!! time to use argp.h?"
-             );
-        exit(1);
-}
-
-/* function to get the user arguments 
- *
- * char *argv[];           command line 
- * int argc;               number of command arguments
- * SMS_AnalParams *pAnalParams;	 analysis parameters structure
- *
- */
-static int GetArguments (char *argv[], int argc, SMS_AnalParams *pAnalParams)
-{
-	int i;
-	float frameRateOrOverlapFactor = 0;
-
-	for (i=1; i<argc-2; i++) 
-	{
-		if (*(argv[i]++) == '-') 
-		{
-			switch (*(argv[i]++)) 
-			{
-/*                         case 'v': verbose = 1; */
-/*                                 break; */
-                        case 'd': if (sscanf(argv[i],"%d", 
-                                             &pAnalParams->iDebugMode) < 0)
-					printf("GetArguments: Invalid debug mode");
-                                break;
-                        case 'f':  if (sscanf(argv[i],"%d", &pAnalParams->iFormat) < 1) 
-					printf("GetArguments: Invalid format");
-                                break;
-                        case 'q': if (sscanf(argv[i],"%d", 
-                                             &pAnalParams->iSoundType) < 0) 
-					printf("GetArguments: Invalid sound type");
-                                break;
-                        case 'x': if (sscanf(argv[i],"%d", 
-                                             &pAnalParams->iAnalysisDirection) < 0) 
-					printf("GetArguments: Invalid value for analysis direction");
-                                break;
-                        case 'i': if (sscanf(argv[i],"%d", 
-                                             &pAnalParams->iWindowType) < 0) 
-					printf("GetArguments: Invalid value for window type");
-                                break;
-                        case 's': if (sscanf(argv[i],"%f", 
-                                             &pAnalParams->fSizeWindow) < 1)
-					printf("GetArguments: Invalid window size");
-                                break;
-                        case 'r':  sscanf(argv[i],"%f", 
-                                          &frameRateOrOverlapFactor);
-                                break;
-                        case 'j': if (sscanf(argv[i],"%f", 
-                                             &pAnalParams->fHighestFreq) < 0) 
-					printf("GetArguments: Invalid highestFreq");
-                                break;
-                        case 'k': if (sscanf(argv[i],"%f", 
-                                             &pAnalParams->fMinPeakMag) < 0) 
-					printf("GetArguments: Invalid minimum peak magnitude");
-                                break;
-                        case 'y': if (sscanf(argv[i],"%d", 
-                                             &pAnalParams->iRefHarmonic) < 1) 
-					printf("GetArguments: Invalid reference harmonic");
-                                break;
-                        case 'm': if (sscanf(argv[i],"%f", 
-                                             &pAnalParams->fMinRefHarmMag) < 0) 
-					printf("GetArguments: Invalid minimum fundamental magnitude");
-                                break;
-                        case 'z': if (sscanf(argv[i],"%f", 
-                                             &pAnalParams->fRefHarmMagDiffFromMax) < 0) 
-					printf("GetArguments: Invalid maximum fundamental magnitude difference \
-								from maximum peak");
-                                break;
-                        case 'l': if (sscanf(argv[i],"%f",
-                                             &pAnalParams->fLowestFundamental) < 1) 
-					printf("GetArguments: Invalid lowest fundamental");
-                                break;
-                        case 'h': if (sscanf(argv[i],"%f",
-                                             &pAnalParams->fHighestFundamental) < 1) 
-					printf("GetArguments: Invalid highest fundamental");
-                                break;
-                        case 'u': if (sscanf(argv[i],"%f", 
-                                             &pAnalParams->fDefaultFundamental) < 1) 
-					printf("GetArguments: Invalid default fundamental");
-                                break;
-                        case 'n': if (sscanf(argv[i],"%d", &pAnalParams->nGuides) < 1) 
-					printf("GetArguments: Invalid number of guides");
-                                break;
-                        case 'p': if (sscanf(argv[i],"%d", 
-                                             &pAnalParams->nTracks) < 1) 
-					printf("GetArguments: Invalid number of tracks");
-                                break;
-                        case 'w': if (sscanf(argv[i],"%f", 
-                                             &pAnalParams->fFreqDeviation) < 0) 
-					printf("GetArguments: Invalid frequency deviation");
-                                break;
-                        case 't': if (sscanf(argv[i],"%f",
-                                             &pAnalParams->fPeakContToGuide) < 0) 
-					printf("GetArguments: Invalid peak contribution to guide");
-                                break;
-                        case 'o': if (sscanf(argv[i],"%f", 
-                                             &pAnalParams->fFundContToGuide) < 0) 
-					printf("GetArguments: Invalid fundamental contribution to guide");
-                                break;
-                        case 'g': if (sscanf(argv[i],"%d", 
-                                             &pAnalParams->iCleanTracks) < 0) 
-					printf("GetArguments: Invalid value for CleanTracks");
-                                break;
-                        case 'a': if (sscanf(argv[i],"%d",
-                                             &pAnalParams->iMinTrackLength) < 0) 
-					printf("GetArguments: Invalid minimum track length");
-                                break;
-                        case 'b': if (sscanf(argv[i],"%d", 
-                                             &pAnalParams->iMaxSleepingTime) < 0) 
-					printf("GetArguments: Invalid minimum sleeping time");
-                                break;
-                        case 'c': if (sscanf(argv[i],"%d",
-                                             &pAnalParams->nStochasticCoeff) < 1) 
-					printf("GetArguments: Invalid number of coefficients");
-                                break;
-                        case 'e': if (sscanf(argv[i],"%d",
-                                             &pAnalParams->iStochasticType) < 0) 
-					printf("GetArguments: Invalid stochastic type");
-                                break;
-                        default:   usage();
-			}
-		}
-	}
-  		
-	if (frameRateOrOverlapFactor > 0.0)
-		pAnalParams->iFrameRate = frameRateOrOverlapFactor;
-	else if (frameRateOrOverlapFactor < 0.0)
-	{ 
-		/* -overlapFactor specified */
-                /* overlap ratio K = windowSize * frameRate / 
-		   defaultFund = w * r / u => r = K * u / w */
-		pAnalParams->iFrameRate = (int) ((-frameRateOrOverlapFactor) * 
-			                             pAnalParams->fDefaultFundamental /
-			                             pAnalParams->fSizeWindow);
-		printf("Frame rate = %d\n", pAnalParams->iFrameRate);
-	}
-
-
-	return (1);
-}
-
 int main (int argc, const char *argv[])
 {
 	FILE *pOutputSmsFile; 
@@ -374,21 +187,27 @@ int main (int argc, const char *argv[])
 
         if(verbose)
         {
-                printf("\n===Sound File Info====\n");
+                printf("\n===sound file info===\n");
                 printf("samples: %d, samplerate: %d, seconds: %f \n", soundHeader.nSamples, soundHeader.iSamplingRate, 
                        soundHeader.nSamples / (float)soundHeader.iSamplingRate);
                 printf("number of channels: %d, read channel: %d \n", soundHeader.channelCount, soundHeader.iReadChannel); 
-                printf("\n===Analysis Parameters====\n");
+                printf("\n===analysis parameters===\n");
 		printf("sizeHop: %d, nFrames: %d \n", analParams.sizeHop, analParams.nFrames);
                 /* \todo: print analysis window type (by name) here */
                 if(analParams.specEnvParams.iType != SMS_ENV_NONE)
                 {
-                        printf("discrete cepstrum spectral envelope parameters: \n");
+                        printf("\n===spectral envelope parameters===\n");
+                        if(analParams.specEnvParams.iType == SMS_ENV_CEP)
+                                printf("type: cepstral coefficients, ");
+                        else if(analParams.specEnvParams.iType == SMS_ENV_FBINS)
+                                printf("type: frequency bins, ");
+                        else
+                                printf("warning: unknown spectral envelope type! \n\n ");
                         printf("order: %d, lambda: %f, max frequency: %d \n", analParams.specEnvParams.iOrder,
                                analParams.specEnvParams.fLambda, analParams.specEnvParams.iMaxFreq);
                 }
-		printf("header info string: %s", smsHeader.pChTextCharacters);
-                printf("\ndoing analysis now:\n");
+		printf("\n===header info string===\n %s", smsHeader.pChTextCharacters);
+                printf("\n\ndoing analysis now:\n");
         }
 
 	while(iDoAnalysis > 0)
