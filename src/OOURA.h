d2d2d0596d577c18d000637db5641e5c35b24eeb
IOhannes m zmölnig <zmoelnig@iem.at>
2024-04-10 08:50:29 +0200

    decorate the OOURA symbols to make them unique
    when building with reduced symbol visibility, this is not really needed.
    but just in case...

--- src/OOURA.h
+++ src/OOURA.h
@@ -12,6 +12,20 @@
 #define NMAX 8192
 #define NMAXSQRT 64
 
+#define DECORATE(x) _sms_ooura_##x
+
+#define rdft DECORATE(rdft)
+#define makewt DECORATE(makewt)
+#define makect DECORATE(makect)
+#define bitrv2 DECORATE(bitrv2)
+#define cftfsub DECORATE(cftfsub)
+#define cftbsub DECORATE(cftbsub)
+#define rftfsub DECORATE(rftfsub)
+#define rftbsub DECORATE(rftbsub)
+#define cft1st DECORATE(cft1st)
+#define cftmdl DECORATE(cftmdl)
+
+
 void rdft(int n, int isgn, sfloat *a, int *ip, sfloat *w);
 
 void makewt(int nw, int *ip, sfloat *w);
